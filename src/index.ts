bc9559b54690fbe29453b6d6276d2a31d85122ec
Kevin Foley <kevinfoley@teachable.com>
2017-11-07 18:50:11 -0500

    Get key mappings set up.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { schema } from 'prosemirror-schema-basic'
 import { EditorState } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
+import { keymap } from 'prosemirror-keymap'
+import { baseKeymap } from 'prosemirror-commands'
 
-const state = EditorState.create({ schema })
+const state = EditorState.create({
+  schema,
+  plugins: [
+    keymap(baseKeymap),
+  ],
+})
 
 new EditorView(
   document.getElementById('app'),
